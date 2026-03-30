5ce1f515384505e75d9e7b6f75ca0d2435435f06
Ange-H0610 <hei.iraka.4@gmail.com>
2026-03-30 13:26:13 +0300

    chore: initial project setup with Spring Boot and Maven

---
+++ src/main/java/com/hei/prog3/Application.java
@@ -0,0 +1,12 @@
+package com.hei.prog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("Restaurant Management API démarrée!");
+    }
+}
